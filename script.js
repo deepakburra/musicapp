b0889948fbdedbb0d54809d355d24d8d10525c28
deepakburra <Deepakburra84@gmail.com>
2024-08-01 07:22:58 -0700

    updated

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let audioElement = new Audio('songs/perfectsong.mp3');
 // audioElement.play(); //this will play automatically when website opens in the background
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
-
+let gif = document.getElementById('gif');
 
 let songs = [
     {songName: "A Thousand Years", filePath: "songs/thousandyears.mp3", coverPath: "assets/thousandyearscoverpic.jpg" },
@@ -22,15 +22,29 @@ masterPlay.addEventListener('click', ()=> {
           audioElement.play()
           masterPlay.classList.remove('fa-circile-play');
           masterPlay.classList.add('fa-circle-pause');
+          gif.style.opacity = 1;
 
     } else {
           audioElement.pause()
           masterPlay.classList.add('fa-circile-play');
           masterPlay.classList.remove('fa-circle-pause');
+          gif.style.opacity = 0;
     }
 })
 
 //Listen to Events
-myProgressBar.addEventListener('timeupdate', (element)=> {
-    console.log(element);
+audioElement.addEventListener('timeupdate', (element)=> {
+    // console.log(element.timeStamp);
+    //Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100); 
+    console.log(progress);
+    myProgressBar.value = progress;
+
+    //If song ends Master button should change and gif hides
+    if(myProgressBar.value == 100) {
+        gif.style.opacity = 0;
+        masterPlay.classList.add('fa-circile-play');
+        masterPlay.classList.remove('fa-circle-pause');
+    }
+    
 })
